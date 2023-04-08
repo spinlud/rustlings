b665bbe4b94cd66d8d34bbc8602e70d132b4da7a
spinlud <ludovico.fabbri@gmail.com>
2023-04-08 23:47:01 +0200

    Init

---
+++ exercises/threads/threads2.rs
@@ -0,0 +1,59 @@
+// threads2.rs
+// Execute `rustlings hint threads2` or use the `hint` watch subcommand for a hint.
+// Building on the last exercise, we want all of the threads to complete their work but this time
+// the spawned threads need to be in charge of updating a shared value: JobStatus.jobs_completed
+
+
+
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+struct JobStatus {
+    jobs_completed: u32,
+}
+
+fn main() {
+    let status = Arc::new(Mutex::new(JobStatus{jobs_completed: 0}));
+    let mut handles = vec![];
+    for _ in 0..10 {
+        let status_shared = Arc::clone(&status);
+        let handle = thread::spawn(move || {
+            thread::sleep(Duration::from_millis(250));
+            // TODO: You must take an action before you update a shared value
+            let mut job_status = status_shared.lock().unwrap();
+            job_status.jobs_completed += 1;
+        });
+        handles.push(handle);
+    }
+
+    let mut completed: u32 = 0;
+
+    // loop {
+    //     completed = status.lock().unwrap().jobs_completed;
+    //
+    //     if completed < 10 {
+    //         println!("Still not done");
+    //         thread::sleep(Duration::from_millis(200));
+    //     }
+    //     else {
+    //         println!("Done {} jobs", completed);
+    //         break
+    //     }
+    // }
+
+    for handle in handles {
+        handle.join().unwrap();
+        completed += 1;
+
+        if completed == 10 {
+            // TODO: Print the value of the JobStatus.jobs_completed. Did you notice anything
+            // interesting in the output? Do you have to 'join' on all the handles?
+            println!("jobs completed {}", status.lock().unwrap().jobs_completed);
+        }
+        else {
+            println!("Not done");
+            thread::sleep(Duration::from_millis(200));
+        }
+    }
+}
